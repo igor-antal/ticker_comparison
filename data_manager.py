1431f249624b3709020ea45c0c4946b8bd9827fb
Igor Antal <149603126+Churak-iii@users.noreply.github.com>
2025-11-02 00:14:54 +0100

    Initial commit

---
+++ data_manager.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import yfinance as yf
+
+
+class DataManager:
+    def __init__(self):
+        self._selected_tickers = []
+        self._ticker_raw_data = {}
+        self._ticker_data_dict = {}
+
+    def set_tickers(self, tickers):
+        self._selected_tickers = tickers
+
+    def get_wealth_index(self, period: int):
+        missing_data = []
+        for ticker in self._selected_tickers:
+            if ticker not in self._ticker_raw_data:
+                missing_data.append(ticker)
+
+        if missing_data:
+            tickers_data = self.fetch_tickers_data(missing_data)
+            for missing_ticker in missing_data:
+                self._ticker_raw_data[missing_ticker] = tickers_data[missing_ticker]
+
+        for ticker in self._selected_tickers:
+            ticker_period_format = ticker + "_" + str(period)
+            if ticker_period_format not in self._ticker_data_dict:
+                raw_data = self._ticker_raw_data[ticker]
+                monthly_data = self.cut_years(period, raw_data)
+                ticker_data = self.create_wealth_index(monthly_data)
+                self._ticker_data_dict[ticker_period_format] = ticker_data
+
+        wealth_index = pd.concat(
+            [self._ticker_data_dict[ticker + "_" + str(period)] for ticker in self._selected_tickers],
+            axis=1)
+        wealth_index.columns = self._selected_tickers
+
+        return wealth_index
+
+    @staticmethod
+    def fetch_tickers_data(tickers):
+        raw_data = yf.download(tickers, period="max", interval="1mo")["Close"]
+        raw_data.index = raw_data.index.to_period("M")
+        return raw_data
+
+    @staticmethod
+    def create_wealth_index(raw_data):
+        ticker_data = raw_data
+        ticker_data["pct_change"] = ticker_data.pct_change()
+        ticker_wealth_index = (1 + ticker_data["pct_change"]).cumprod()
+        ticker_wealth_index = ticker_wealth_index.dropna()
+
+        first_row = pd.Series(index=(ticker_wealth_index.index.shift(-1)[0],), data=(1,))
+        ticker_wealth_index = pd.concat((first_row, ticker_wealth_index), axis=0)
+        return ticker_wealth_index
+
+    @staticmethod
+    def ticker_analysis(wealth_index):
+        ticker = wealth_index.columns[0]
+        wealth_index["peaks"] = wealth_index[ticker].cummax()
+        wealth_index["downturn"] = wealth_index[ticker] / wealth_index["peaks"] - 1
+        return wealth_index
+
+    @staticmethod
+    def cut_years(years_to_cut: int | None, data: pd.DataFrame):
+        print(data.index, type(data.index))
+        cut_data = data.loc[data.index >= data.index.max() - years_to_cut] if years_to_cut else data
+        return cut_data
+
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+from tkinter import OptionMenu, StringVar, messagebox
+from tkinter import Tk, ttk
+import matplotlib.pyplot as plt
+from data_manager import DataManager
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+        self._dm = DataManager()
+
+        self.geometry("900x600")
+        self.title("Ticker Comparison Tool")
+
+        self._main_frame = ttk.Frame(self)
+        self._main_frame.pack(fill="both", expand=True)
+        self._main_frame.rowconfigure(1, weight=1)
+        self._main_frame.columnconfigure(0, weight=1)
+        self._main_frame.columnconfigure(1, weight=1)
+
+        self._ticker_input = tk.Text(self._main_frame, width=40, height=5)
+        self._ticker_input.grid(column=0, row=0)
+
+        self._canvas = PlotCanvas(self._main_frame)
+        self._canvas.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+
+        ttk.Button(self._main_frame, text="Plot Stuff", command=self.execute).grid(column=1, row=1)
+
+        self._period_options = ["Maximum", "10 years", "5 years", "1 year"]
+        self._selected = StringVar(value="Maximum")
+        OptionMenu(self._main_frame, self._selected, *self._period_options).grid(column=1, row=0)
+
+    def execute(self):
+        u_input = (self._ticker_input.get("1.0", tk.END).strip()
+                   .replace(" ", "").upper().split(","))
+
+        try:
+            self._dm.set_tickers(u_input)
+            choice_map = {"Maximum": None,
+                          "10 years": 120,
+                          "5 years": 60,
+                          "1 year": 12}
+
+            tickers_data = self._dm.get_wealth_index(choice_map[self._selected.get()])
+
+            self._canvas.plot_wealth_index(tickers_data)
+
+        except ValueError as e:
+            tk.messagebox.showerror("Error", str(e))
+
+
+class PlotCanvas(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.canvas_widget = None
+
+    def plot_wealth_index(self, wealth_index):
+        if self.canvas_widget:
+            self.canvas_widget.get_tk_widget().destroy()
+
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.set_ylabel("Wealth Index")
+        ax.grid(True)
+        ax.set_title("Wealth Index Comparison")
+        wealth_index.plot(ax=ax)
+
+        self.canvas_widget = FigureCanvasTkAgg(fig, master=self)
+        self.canvas_widget.draw()
+        self.canvas_widget.get_tk_widget().pack(fill="both", expand=True)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
